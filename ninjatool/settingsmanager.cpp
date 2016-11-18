d30e3ea4abe93021b78122abec89c83599e06927
TheFatNinja <nbollom@gmail.com>
2016-11-18 12:06:42 +1100

    Some work on SettingsManager for persisting/loading ninjatool settings

--- ninjatool/settingsmanager.cpp
+++ ninjatool/settingsmanager.cpp
@@ -4,22 +4,107 @@
 
 #include "settingsmanager.h"
 #include <rapidjson/filereadstream.h>
+#include <rapidjson/filewritestream.h>
+#include <rapidjson/writer.h>
+#include <QtCore/QtCore>
 
 using namespace rapidjson;
 using namespace std;
 
-SettingsManager::SettingsManager() {
-    FILE* fp = fopen("settings.json", "r");
-    char readBuffer[512];
-    FileReadStream is(fp, readBuffer, sizeof(readBuffer));
-    d.ParseStream(is);
+string SettingsManager::filePath = "";
+Document SettingsManager::d;
+
+inline string stringFromQString(QString value) {
+    QByteArray ba = value.toLatin1();
+    const char *bytes = ba;
+    return string(bytes);
 }
 
-SettingsManager *SettingsManager::instance = nullptr;
-
-SettingsManager* SettingsManager::GetInstance() {
-    if (instance == nullptr) {
-        instance = new SettingsManager();
+void SettingsManager::Init(QString file) {
+    filePath = stringFromQString(file);
+    FILE* fp = fopen(filePath.c_str(), "r");
+    if (fp) {
+        char readBuffer[512];
+        FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+        d.ParseStream(is);
+        fclose(fp);
     }
-    return instance;
+    else {
+        d.SetObject(); //initialize a default settings object and persist
+//        Sync();
+    }
+}
+
+void SettingsManager::Free() {
+    Sync();
+}
+
+bool SettingsManager::Sync() {
+    FILE* fp = fopen(filePath.c_str(), "w+");
+    if (fp) {
+        char writeBuffer[512];
+        FileWriteStream os(fp, writeBuffer, sizeof(writeBuffer));
+        Writer<FileWriteStream> writer(os);
+        d.Accept(writer);
+        fclose(fp);
+        return true;
+    }
+    return false;
+}
+
+Value* SettingsManager::processKeySections(QString key, bool create) {
+    if (!key.length()) {
+        return nullptr;
+    }
+    QStringList parts = key.split(".");
+    Value *v = &d;
+    for (QStringList::iterator i = parts.begin(); i != parts.end(); ++i) {
+        string part = stringFromQString(*i);
+        if (!(v->IsObject() && v->HasMember(part.c_str()))) {
+            if (create) {
+                if (!v->IsObject()) {
+                    v->SetObject(); // Make sure its an object so we can create a member on it
+                }
+                Value k(part.c_str(), (int)part.length(), d.GetAllocator());
+                Value nv;
+                v->AddMember(k, nv, d.GetAllocator());
+            }
+            else {
+                v = nullptr;
+                break;
+            }
+        }
+        v = &(*v)[part.c_str()];
+    }
+    return v;
+}
+
+bool SettingsManager::GetBool(QString key, bool defaultValue) {
+    Value *v = processKeySections(key);
+    if (v == nullptr || !v->IsBool()) {
+        return defaultValue;
+    }
+    else {
+        return v->GetBool();
+    }
+}
+
+void SettingsManager::SetBool(QString key, bool value) {
+    Value *v = processKeySections(key, true);
+    v->SetBool(value);
+}
+
+string SettingsManager::GetString(QString key, string defaultValue) {
+    Value *v = processKeySections(key);
+    if (v == nullptr || !v->IsString()) {
+        return defaultValue;
+    }
+    else {
+        return v->GetString();
+    }
+}
+
+void SettingsManager::SetString(QString key, std::string value) {
+    Value *v = processKeySections(key, true);
+    v->SetString(value.c_str(), (int)value.length(), d.GetAllocator());
 }
--- ninjatool/settingsmanager.h
+++ ninjatool/settingsmanager.h
@@ -6,18 +6,26 @@
 #define PROJECT_SETTINGSMANAGER_H
 
 #include <rapidjson/document.h>
+#include <QString>
+#include <list>
+#include <string>
+
+#define DEFINE_GET_SET(type, get_name, set_name) static type get_name(QString key, type defaultValue); static void set_name(QString key, type value);
 
 class SettingsManager {
 
 private:
-    SettingsManager();
-    static SettingsManager *instance;
-    rapidjson::Document d;
+    SettingsManager() {};
+    static rapidjson::Document d;
+    static std::string filePath;
+    static rapidjson::Value* processKeySections(QString key, bool create = false);
 
 public:
-    static SettingsManager* GetInstance();
-
-
+    static void Init(QString file);
+    static void Free();
+    static bool Sync();
+    DEFINE_GET_SET(bool, GetBool, SetBool)
+    DEFINE_GET_SET(std::string, GetString, SetString)
 };
 
 #endif //PROJECT_SETTINGSMANAGER_H
